1c161fba998eb06eb015182180f9b061b12e6cde
Jeffrey Taylor <email@jeffreytaylor.org>
2013-11-30 19:54:24 -0800

    added router code

--- framework/framework.router.js
+++ framework/framework.router.js
@@ -1,1 +1,86 @@
-// router coming soon
+
+// to do --
+// 1) add query string parsing
+// 2) add fallbacks for browsers that don't support push/popstate
+
+
+var router = (function () {
+
+    var _routes = [];
+
+    var parseRoute = function (path) {
+
+        var nameRegex = new RegExp(":([^/.\\\\]+)", "g"),
+            newRegex = "" + path;
+
+
+        var values = nameRegex.exec(path);
+
+        if (values != null) {
+
+            newRegex = newRegex.replace(values[0],"([^/.\\\\]+)");
+        }
+
+        newRegex = newRegex + '$';
+
+        return {regex: new RegExp(newRegex)};
+
+    };
+
+    var matchRoute = function (url) {
+
+        for (var i = 0; i < _routes.length; i++) {
+            var route = _routes[i],
+                match = route.params.regex.exec(url);
+
+            if (!match) {
+                continue;
+            }
+
+            route.callback({url: url})
+            return true;
+        }
+
+        return false;
+
+    };
+
+    var start = function () {
+
+        var loaded = false;
+
+        if (loaded == false) {
+            matchRoute(document.location.pathname);
+            loaded = true;
+        }
+
+        window.addEventListener("popstate", function(e) {
+
+            matchRoute(document.location.pathname);
+
+        });
+
+        window.addEventListener("load", function(e) {
+
+            if(loaded == false) {
+                matchRoute(document.location.pathname);
+                loaded = true;
+            }
+
+        });
+
+        window.addEventListener("hashchange", function(e) {
+
+            matchRoute(document.location.pathname);
+        });
+    };
+
+    start();
+
+    return  {
+       addRoute: function (route, callback) {
+           _routes.push({params: parseRoute(route), callback: callback})
+       }
+    };
+
+})();
