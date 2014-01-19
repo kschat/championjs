558b34edf1df8cb1b1d9d537fa909e365c354ba1
kschat <kyleschattler@gmail.com>
2014-01-19 13:15:36 -0500

    Added features and tests for events.js

--- src/event.js
+++ src/event.js
@@ -1,28 +1,46 @@
 var events = champ.events = (function () {
-    var _subscribers = {};
+    var _subscribers = {},
+        _defaultPriority = 10;
     
     return {
         trigger: function (topic, data) {
             var subs = _subscribers[topic] || [];
-
-            for (var sub in subs) {
-                subs[sub](data, topic);
+            for (var s in subs) {
+                for(var p in subs[s]) { subs[s][p].handler(data, topic); }
             }
+
+            return this;
         },
 
-        on: function (topic, handler) {
+        on: function (topic, handler, options) {
+            var priority = (options || {}).priority || _defaultPriority,
+                context = (options || {}).context || this;
+
             _subscribers[topic] = _subscribers[topic] || [];
-            _subscribers[topic].push(handler);
+            _subscribers[topic][priority] = _subscribers[topic][priority] || [];
+            handler = typeof handler === 'function' ? [handler] : handler;
+
+            for(var i=0; i<handler.length; i++) {
+                _subscribers[topic][priority].push({ 
+                    handler: handler[i].bind(context),
+                    id: handler[i]
+                });
+            }
+
+            return this;
         },
 
         off: function (topic, handler) {
+            _subscribers = arguments.length === 0 ? {} : _subscribers;
             var subs = _subscribers[topic] || [];
-            
-            for (var i = 0; i < subs.length; i++) {
-                if ('' + subs[i] == '' + handler) {
-                    subs.splice(i, 1);
+
+            for(var s in subs) {
+                for(var p in subs[s]) {
+                    if(subs[s][p].id == handler) { subs[s].splice(p, 1); }
                 }
             }
+
+            return this;
         }
     };
 })();
