14d6e114b833a19bae0ff165a7e1d2fc60650314
Kyle Schattler <kyleschattler@gmail.com>
2014-05-28 21:41:07 -0400

    Release v0.2.1

--- champion.js
+++ champion.js
@@ -1,5 +1,5 @@
 /*
- *  champion.js - 0.1.0
+ *  champion.js - 0.2.1
  *  Contributors: Kyle Schattler, Jeff Taylor
  *  Description: Yet another frontend MVP JS framework
  *  Source: https://github.com/kschat/championjs.git
